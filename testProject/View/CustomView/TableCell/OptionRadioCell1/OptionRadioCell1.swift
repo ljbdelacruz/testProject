e622d3cc89e8ae9308030d27b4ab605dc63a150d
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-08-08 07:27:11 +0800

    updated

---
+++ testProject/View/CustomView/TableCell/OptionRadioCell1/OptionRadioCell1.swift
@@ -0,0 +1,24 @@
+//
+//  OptionRadioCell1.swift
+//  testProject
+//
+//  Created by Lainel John Dela Cruz on 07/08/2019.
+//  Copyright © 2019 devops. All rights reserved.
+//
+
+import UIKit
+
+class OptionRadioCell1: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
