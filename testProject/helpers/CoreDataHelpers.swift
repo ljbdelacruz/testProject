1e7d55b20973a402515240664552fa60ec4c905a
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-08-13 07:19:33 +0800

    added example in coredatahelpers

--- testProject/helpers/CoreDataHelpers.swift
+++ testProject/helpers/CoreDataHelpers.swift
@@ -58,3 +58,35 @@ class CoreDataHelpers{
     }
     
 }
+
+
+//MARK: Examples
+
+extension CoreDataHelpers{
+    func exampleSave(){
+        let category=self.setTableEntity(table: "Category")
+        category.setValue(10, forKey: "parent")
+        category.setValue(0, forKey: "id")
+        category.setValue("test10", forKey: "name");
+        self.saveContext();
+    }
+    func exampleRead(){
+        let data=self.fetchRequestTable(table: "Category") as! [Category];
+        let objectToDelete=data[0] as! NSManagedObject;
+        print(data.count);
+    }
+    func exampleDelete(){
+        var data=self.fetchRequestTable(table: "Category") as! [Category];
+        let toDeleteObject=data[0] as! NSManagedObject
+        self.context!.viewContext.delete(toDeleteObject);
+        self.saveContext();
+    }
+    func exampleUpdate(){
+        var data=self.fetchRequestTable(table: "Category") as! [Category];
+        data[0].parent=2;
+        self.saveContext();
+    }
+    
+}
+
+
