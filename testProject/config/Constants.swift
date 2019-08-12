d2a8f9a1df8831b8ad6ac1d5399f42ac292c9844
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-08-12 13:48:09 +0800

    added reference enpoints

---
+++ testProject/config/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  testProject
+//
+//  Created by Lainel John Dela Cruz on 12/08/2019.
+//  Copyright © 2019 devops. All rights reserved.
+//
+
+import Foundation
+
+
+public class Constants{
+    
+    
+    
+    
+    
+    //MARK: Strip config
+    public static var stripePerishKey:String="pk_test_DDGi6KsUGJIg3lQUCA9Gdmyw00zeTFsAnh";
+    
+}
---
+++ testProject/integrations/StripeIntegration.swift
@@ -0,0 +1,25 @@
+//
+//  StripPayments.swift
+//  testProject
+//
+//  Created by Lainel John Dela Cruz on 12/08/2019.
+//  Copyright © 2019 devops. All rights reserved.
+//
+
+import Foundation
+
+/*
+ testing cards must have web server process the payment all the app can do is pass the necessary properties
+ that you require from the client and the web service process the payments from the strip or any payment
+ platforms you used and returns the response from the app
+ 
+ Source: https://stripe.com/docs/testing#cards
+*/
+
+
+
+
+public class StripIntegration{
+    public let perishableKey:String="";
+    
+}
