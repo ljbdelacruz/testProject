c29953e159be33273132017bdac41b4523987704
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-08-16 18:47:33 +0800

    updated shits'

---
+++ testProject/Components/TransactionList/TransactionList.swift
@@ -0,0 +1,21 @@
+//
+//  TransactionList.swift
+//  testProject
+//
+//  Created by devops on 16/08/2019.
+//  Copyright © 2019 devops. All rights reserved.
+//
+
+import UIKit
+
+class TransactionList: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
